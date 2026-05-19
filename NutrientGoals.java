9e89fbda2e6e959d62bbf43320c3a277f5e9395f
Sam Taheri <samtaheri1000@gmail.com>
2026-05-19 15:46:19 -0400

    daily nutrient goal targets

---
+++ NutrientGoals.java
@@ -0,0 +1,35 @@
+class NutrientGoals {
+    private int calorieGoal;
+    private double proteinGoal;
+    private double carbsGoal;
+    private double fatGoal;
+    private double fiberGoal;
+    private double sugarGoal;
+
+    public NutrientGoals(int calorieGoal, double proteinGoal, double carbsGoal,
+                         double fatGoal, double fiberGoal, double sugarGoal) {
+        this.calorieGoal = calorieGoal;
+        this.proteinGoal = proteinGoal;
+        this.carbsGoal = carbsGoal;
+        this.fatGoal = fatGoal;
+        this.fiberGoal = fiberGoal;
+        this.sugarGoal = sugarGoal;
+    }
+
+    public int getCalorieGoal()   { return calorieGoal; }
+    public double getProteinGoal() { return proteinGoal; }
+    public double getCarbsGoal()   { return carbsGoal; }
+    public double getFatGoal()     { return fatGoal; }
+    public double getFiberGoal()   { return fiberGoal; }
+    public double getSugarGoal()   { return sugarGoal; }
+
+    public void displayGoals() {
+        System.out.println("\n----- Daily Nutrient Goals -----");
+        System.out.println("Calorie goal: " + calorieGoal + " cal");
+        System.out.println("Protein goal: " + proteinGoal + " g");
+        System.out.println("Carbs goal:   " + carbsGoal + " g");
+        System.out.println("Fat goal:     " + fatGoal + " g");
+        System.out.println("Fiber goal:   " + fiberGoal + " g");
+        System.out.println("Sugar goal:   " + sugarGoal + " g");
+    }
+}
