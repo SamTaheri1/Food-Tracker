a82a98bba6331299bc94ed5a52564f147e05701c
Sam Taheri <samtaheri1000@gmail.com>
2026-05-25 19:25:11 -0400

    added default goals

--- NutrientGoals.java
+++ NutrientGoals.java
@@ -6,6 +6,15 @@ class NutrientGoals {
     private double fiberGoal;
     private double sugarGoal;
 
+    public NutrientGoals() {
+        calorieGoal = 2000;
+        proteinGoal = 100;
+        carbsGoal = 250;
+        fatGoal = 70;
+        fiberGoal = 25;
+        sugarGoal = 50;
+    }
+
     public NutrientGoals(int calorieGoal, double proteinGoal, double carbsGoal,
                          double fatGoal, double fiberGoal, double sugarGoal) {
         this.calorieGoal = calorieGoal;
@@ -16,20 +25,37 @@ class NutrientGoals {
         this.sugarGoal = sugarGoal;
     }
 
-    public int getCalorieGoal()   { return calorieGoal; }
-    public double getProteinGoal() { return proteinGoal; }
-    public double getCarbsGoal()   { return carbsGoal; }
-    public double getFatGoal()     { return fatGoal; }
-    public double getFiberGoal()   { return fiberGoal; }
-    public double getSugarGoal()   { return sugarGoal; }
+    public int getCalorieGoal() {
+        return calorieGoal;
+    }
+
+    public double getProteinGoal() {
+        return proteinGoal;
+    }
+
+    public double getCarbsGoal() {
+        return carbsGoal;
+    }
+
+    public double getFatGoal() {
+        return fatGoal;
+    }
+
+    public double getFiberGoal() {
+        return fiberGoal;
+    }
+
+    public double getSugarGoal() {
+        return sugarGoal;
+    }
 
     public void displayGoals() {
         System.out.println("\n----- Daily Nutrient Goals -----");
         System.out.println("Calorie goal: " + calorieGoal + " cal");
         System.out.println("Protein goal: " + proteinGoal + " g");
-        System.out.println("Carbs goal:   " + carbsGoal + " g");
-        System.out.println("Fat goal:     " + fatGoal + " g");
-        System.out.println("Fiber goal:   " + fiberGoal + " g");
-        System.out.println("Sugar goal:   " + sugarGoal + " g");
+        System.out.println("Carbs goal: " + carbsGoal + " g");
+        System.out.println("Fat goal: " + fatGoal + " g");
+        System.out.println("Fiber goal: " + fiberGoal + " g");
+        System.out.println("Sugar goal: " + sugarGoal + " g");
     }
 }
