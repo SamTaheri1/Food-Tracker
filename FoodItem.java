cab6a695258b8047b4579d1d9121a287d1d54c0a
Sam Taheri <samtaheri1000@gmail.com>
2026-06-02 16:48:11 -0400

    added note and favorite functionality

--- FoodItem.java
+++ FoodItem.java
@@ -3,12 +3,16 @@ class FoodItem {
     private int calories;
     private Nutrients nutrients;
     private String category;
+    private String note;
+    private boolean favorite;
 
     public FoodItem(String foodName, int calories, Nutrients nutrients, String category) {
         this.foodName = foodName;
         this.calories = calories;
         this.nutrients = nutrients;
         this.category = category;
+        note = "No note";
+        favorite = false;
     }
 
     public String getFoodName() {
@@ -27,6 +31,14 @@ class FoodItem {
         return category;
     }
 
+    public String getNote() {
+        return note;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
     public void setFoodName(String foodName) {
         this.foodName = foodName;
     }
@@ -43,10 +55,29 @@ class FoodItem {
         this.category = category;
     }
 
+    public void setNote(String note) {
+        if (note == null || note.trim().length() == 0) {
+            this.note = "No note";
+        }
+        else {
+            this.note = note;
+        }
+    }
+
+    public void markFavorite() {
+        favorite = true;
+    }
+
+    public void unmarkFavorite() {
+        favorite = false;
+    }
+
     public void displayFood() {
         System.out.println("Food name: " + foodName);
         System.out.println("Category: " + category);
         System.out.println("Calories: " + calories);
+        System.out.println("Favorite: " + favorite);
+        System.out.println("Note: " + note);
         nutrients.displayNutrients();
     }
 
